ce8664f2318cd59f21c0240b03d710c5841c235b
Mrfroxi <pilutikas340@gmail.com>
2026-06-07 16:34:39 +0300

    feat:setup microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options:{
+      package: 'auth.v1',
+      protoPath: 'node_modules/@prismcinema/contracts/proto/auth.proto',
+      url: 'localhost:50051',
+      logging: {
+        keepCase:false,
+        longs:String,
+        enums:String,
+        default:true,
+        oneofs:true,
+      }
+    }
+  })
+
+
+  await app.startAllMicroservices()
+  await app.init() //initialisation di container
 
 }
 bootstrap();
