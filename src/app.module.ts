710acf4ed0156357e8bcdcb8390b4271a978cac2
Mrfroxi <pilutikas340@gmail.com>
2026-06-10 17:03:44 +0300

    feat:add and setup redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import {ConfigModule} from "@nestjs/config";
 import { RedisModule } from './infrastructure/redis/redis.module';
+import { OtpModule } from './modules/otp/otp.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, PrismaModule, RedisModule],
+  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, PrismaModule, RedisModule, OtpModule],
   controllers: [],
   providers: [],
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,31 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import type {SendOtpRequest} from "@prismcinema/contracts/gen/auth";
-import {AuthRepository} from "@/modules/auth/auth.repository";
-import {Account} from "prisma/generated/client";
+import { Injectable } from "@nestjs/common";
+import type { SendOtpRequest } from "@prismcinema/contracts/gen/auth";
+import { AuthRepository } from "@/modules/auth/auth.repository";
+import { Account } from "prisma/generated/client";
+import { OtpService } from "@/modules/otp/otp.service";
 
 @Injectable()
 export class AuthService {
-    public constructor(private readonly authRepo: AuthRepository) {
+  public constructor(
+    private readonly authRepo: AuthRepository,
+    private readonly otpService: OtpService,
+  ) {}
+
+  public async sendOtp(data: SendOtpRequest) {
+    const { identifier, type } = data;
+
+    let account: Account | null;
+
+    if (type === "phone") account = await this.authRepo.findByPhone(identifier);
+    else account = await this.authRepo.findByEmail(identifier);
+
+    if (!account) {
+      account = await this.authRepo.createAccount({
+        phone: type === "phone" ? identifier : null,
+        email: type === "email" ? identifier : null,
+      });
     }
 
-    public async sendOtp(data:SendOtpRequest){
+    const code = await this.otpService.send(
+      identifier,
+      type as "phone" | "email",
+    );
 
-        const { identifier, type} = data
+    console.debug("Code:", code);
 
-
-        let account : Account | null
-
-        if(type === "phone") account = await this.authRepo.findByPhone(identifier)
-        else account = await  this.authRepo.findByEmail(identifier)
-
-        if(!account){
-            account = await this.authRepo.createAccount({
-                phone: type === 'phone' ? identifier : null,
-                email: type === 'email' ? identifier : null
-            })
-        }
-
-
-        return {ok:true}
-    }
+    return { ok: true };
+  }
 }
