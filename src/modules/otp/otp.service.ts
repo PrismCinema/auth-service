ff8e77d4162fad5690a21e3629149eefb09d4a8d
Mrfroxi <pilutikas340@gmail.com>
2026-06-10 17:07:46 +0300

    feat:add patcode

--- src/modules/otp/otp.service.ts
+++ src/modules/otp/otp.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { RedisService } from "@/infrastructure/redis/redis.service";
 import { createHash } from "node:crypto";
-
+import { generateCode } from "patcode";
 @Injectable()
 export class OtpService {
   public constructor(private readonly redisService: RedisService) {}
@@ -9,14 +9,13 @@ export class OtpService {
   public async send(identifier: string, type: "phone" | "email") {
     const { code, hash } = this.generateCode();
 
-
     await this.redisService.set(`otp${type} : ${identifier}`, hash, "EX", 300);
 
     return code;
   }
 
   private generateCode() {
-    const code = Math.floor(100000 + Math.random() * 900000);
+    const code = generateCode();
     const hash = createHash("sha256").update(String(code)).digest("hex");
 
     return { code, hash };
